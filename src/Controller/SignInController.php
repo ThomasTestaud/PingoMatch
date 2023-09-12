e13857f49a18c0265c13a8dd83ddba51d33f2855
*Stratt321963 <thomas.testaud@hotmail.fr>
2023-09-12 14:14:49 +0200

    ...

--- src/Controller/SignInController.php
+++ src/Controller/SignInController.php
@@ -8,11 +8,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SignInController extends AbstractController
 {
-    #[Route('/sign/in', name: 'app_sign_in')]
+    #[Route('/signin', name: 'app_sign_in')]
     public function index(): Response
     {
+        $file_path = '/src/JSON/users.json';
+        $jsonFilePath = $this->getParameter('kernel.project_dir') . '/src/JSON/users.json';
+        $users = json_decode(file_get_contents($jsonFilePath));
+
+        $return = "";
+        if(isset($_POST['login']) && isset($_POST['password'])) {
+            $users[] = [
+                "Name" => $_POST['login'],
+                "img" => "https://robotdog.thomastestaud.com/mancho/h.png",
+                "Password" => $_POST['password'],
+                "Description" => $_POST['description']
+            ];
+
+            file_put_contents($jsonFilePath, json_encode($users));
+
+            header("Location: /");
+            exit;
+        } else {
+            $return = "Vous devez remplir tout les champs";
+        }
+
+
         return $this->render('sign_in/index.html.twig', [
-            'controller_name' => 'SignInController',
+            'result' => $return,
         ]);
     }
 }
